4b7d73585e1b3c1a0a93ae8e2e96dac0c16906a2
mizphses <fuminori@mizphses.com>
2021-12-12 03:23:56 +0900

    feat: addFadeIn and addCountDown

--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,16 @@
 <template>
   <div>
-      <input v-model="username" type="input" class="input sans" placeholder="ここに名前を入力するとスクショに名前が出ます" />
-    <div class="MainModule">
+    <div v-if="$dayjs('2020-02-04').diff('2020-01-01')===0" class="MainModule">
       <div>
-        <span class="fude is-size-2">{{username}}さんへ</span>
+        <span v-text="`あと`+-1*$dayjs().diff('2022-01-01', 'day')+'日で公開'" />
       </div>
-      <AkeomeModule class="MainImage" @message-data="messageData($event, message)"/>
-      <a :href="createUrl">Tweet</a>
-      <NuxtContent :document="akeomessage" class="serif article"/>
+    </div>
+    <div v-else>
+      <a class="button" href="/happyNewYear">明けましておめでとうございます。（年賀状サイトへ）</a>
     </div>
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({
-  async asyncData ({ $content }) {
-    const akeomessage = await $content('akeomessage').fetch()
-
-    return {
-      akeomessage
-    }
-  },
-  data() {
-    return {
-      username: "",
-      message: "",
-      createUrl: "",
-    }
-  },
-  mounted() {
-    this.createUrl = "https://twitter.com/intent/tweet?text=なとさんから，2022年元旦のご挨拶（年賀状）&url=" + encodeURIComponent(window.location.href + "share?="+ this.message + "&name=" + this.username)
-  },
-  methods: {
-    messageData(message: "") {
-      this.message = message
-    }
-  }
-})
-</script>
 <style lang="scss">
 
 $PaddingTnB: 100px;
@@ -77,5 +48,18 @@ body{
 }
 .is-size-2{
   font-size: 4em;
+}
+.button{
+  font-size: 1.5em;
+  position: absolute;
+  top: 100px;
+  padding: 1em;
+  margin: 1em;
+  border: 1px solid rgb(80, 80, 80);
+  text-decoration: none;
+  color: black;
+}
+.button:hover{
+  background: rgb(253, 255, 130);
 }
 </style>
